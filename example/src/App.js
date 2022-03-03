83b5127ec856e99c8a14d6ae7ce99650e59590d0
sajandhakal12 <sajan12dhakal@gmail.com>
2022-03-03 14:37:54 +0545

    fix: forward ref

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 import NepaliInput from 'react-nepali-input'
 
 const App = () => {
+  const inputRef = useRef()
   const [value, setValue] = React.useState('')
-  return <NepaliInput value={value} setValue={(value) => setValue(value)} />
+  return (
+    <NepaliInput
+      value={value}
+      setValue={(value) => setValue(value)}
+      ref={inputRef}
+    />
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,35 +1,41 @@
 import React from 'react'
 import { setUnicode } from './nepali'
 
-const NepaliInput = ({
-  getInputData,
-  fontType = 'traditional',
-  language = 'nepali',
-  onChange,
-  onKeyPress,
-  setValue,
-  ...rest
-}) => {
-  return (
-    <input
-      {...rest}
-      onChange={(e) => {
-        onChange && onChange(e)
-        setValue && setValue(e.target.value)
-      }}
-      onKeyPress={(e) => {
-        if (language === 'nepali') {
-          if (fontType === 'traditional' || fontType === 'romanized') {
-            const { value, newEndPos } = setUnicode(e, fontType)
-            e.target.value = value
-            e.target.setSelectionRange(newEndPos, newEndPos)
+const NepaliInput = React.forwardRef(
+  (
+    {
+      getInputData,
+      fontType = 'traditional',
+      language = 'nepali',
+      onChange,
+      onKeyPress,
+      setValue,
+      ...rest
+    },
+    ref
+  ) => {
+    return (
+      <input
+        {...rest}
+        onChange={(e) => {
+          onChange && onChange(e)
+          setValue && setValue(e.target.value)
+        }}
+        onKeyPress={(e) => {
+          if (language === 'nepali') {
+            if (fontType === 'traditional' || fontType === 'romanized') {
+              const { value, newEndPos } = setUnicode(e, fontType)
+              e.target.value = value
+              e.target.setSelectionRange(newEndPos, newEndPos)
+            }
+            onKeyPress && onKeyPress(e)
+            setValue && setValue(e.target.value)
           }
-        }
-        onKeyPress && onKeyPress(e)
-        setValue && setValue(e.target.value)
-      }}
-    />
-  )
-}
+        }}
+        ref={ref}
+      />
+    )
+  }
+)
 
 export default NepaliInput
